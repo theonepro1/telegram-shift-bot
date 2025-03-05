7aa31d5297ab2903f91eab528d291d41939cf5bb
theonepro1 <a.perneyev@gmail.com>
2025-03-05 18:56:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 DATA_FILE = os.getenv("DATA_FILE", "data.json")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # ID Аскара для выгрузки отчетов
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -30,7 +31,7 @@ def get_current_shift():
     shift_type = "ДЕНЬ" if now.hour < 20 else "НОЧЬ"
     return now.strftime("%d.%m.%y"), shift_type
 
-# Обновление смены после подтверждения
+# Обновление смены
 def update_shift():
     today, shift = get_current_shift()
     if shift == "ДЕНЬ":
@@ -40,9 +41,9 @@ def update_shift():
         today = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d.%m.%y")
     return today, next_shift
 
-# Операторы и бармены (для выбора)
+# Операторы и бармены
 operators = ["Ардина", "Назгул", "Жазира"]
-barmen = ["Дастан", "Магжан", "Мейржан",]
+barmen = ["Дастан", "Магжан", "Мейржан"]
 
 # Клавиатуры
 confirm_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(
@@ -65,22 +66,25 @@ async def start(message: types.Message):
 
 @dp.message_handler(lambda message: message.text in operators)
 async def operator_selected(message: types.Message):
-    await message.answer(f"Оператор {message.text}, подтвердите сдачу смены.", reply_markup=confirm_keyboard)
+    await message.answer(f"Оператор {message.text}, введите поступления по ПК, SimRacing, PlayStation, затем остаток в кассе.")
 
 @dp.message_handler(lambda message: message.text == "✅ Подтверждаю")
 async def confirm_shift(message: types.Message):
     today, shift = get_current_shift()
     next_day, next_shift = update_shift()
-    await message.answer(f"Кутты болсын :-0, смена ({today} - {shift}) ЗАКРЫЛАСЬ.\nОткрылась смена ({next_day} - {next_shift}).")
-    await message.answer("Бармен, подтвердите остатки напитков:", reply_markup=barmen_keyboard)
+    await message.answer(f"Смена ({today} - {shift}) ЗАКРЫЛАСЬ.\nОткрылась смена ({next_day} - {next_shift}).")
+    await message.answer("Бармен, введите поступления по бару, остатки еды, напитков и остаток в кассе:", reply_markup=barmen_keyboard)
 
 @dp.message_handler(lambda message: message.text in barmen)
 async def barmen_selected(message: types.Message):
-    await message.answer(f"Бармен {message.text}, подтвердите остатки напитков.")
+    await message.answer(f"Бармен {message.text}, подтвердите остатки напитков и кассы.")
 
-@dp.message_handler(lambda message: message.text == "❌ Отмена")
-async def cancel_shift(message: types.Message):
-    await message.answer("Отмена подтверждения смены.")
+@dp.message_handler(commands=['report'])
+async def send_report(message: types.Message):
+    if message.from_user.id == ADMIN_ID:
+        await message.answer("Выгружаю отчет...")
+    else:
+        await message.answer("У вас нет доступа к отчетам.")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
